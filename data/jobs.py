1e36b63440ee9c87e977edd68915bab26259b07b
ffedor <ffedor.ffedorov2005@gmail.com>
2022-02-28 21:15:53 +0300

    problem4

--- data/jobs.py
+++ data/jobs.py
@@ -3,15 +3,14 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 
 
-class Job(SqlAlchemyBase):
+class Jobs(SqlAlchemyBase):
     __tablename__ = 'jobs'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
-    job = sqlalchemy.Column(sqlalchemy.String)
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    collaborations = sqlalchemy.Column(sqlalchemy.PickleType)
-    start_time = sqlalchemy.Column(sqlalchemy.Integer)
-    end_time = sqlalchemy.Column(sqlalchemy.Integer)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    collaborations = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_date = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
--- data/main.py
+++ data/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -15,6 +17,13 @@ def main():
              "speciality": ["research engineer", "programmer", "chef", "geographer"],
              "address": ["module_1" for i in range(4)],
              "email": ["scott_chief@mars.org", "Vladimir.E@mars.org", "22Petr22@mars.org", "yayan@mars.org"]}
+    jobs_data = {"team_leader": [1, 1, 1, 1],
+                 "job": ["deployment of residential modules 1 and 2", "cleaning modules 1 and 2",
+                         "cooking", "determining the route"],
+                 "work_size": [15, 20, 20, 20],
+                 "collaborations": ["2, 3", "1, 3", "1, 2", "1, 2, 3"],
+                 "start_date": [datetime.datetime.now() for i in range(4)],
+                 "is_finished": [False, False, False, False]}
     db_sess = db_session.create_session()
     for num in range(4):
         user = User()
@@ -26,6 +35,15 @@ def main():
         user.address = users["address"][num]
         user.email = users["email"][num]
         db_sess.add(user)
+
+        jobs = Jobs()
+        jobs.team_leader = jobs_data["team_leader"][num]
+        jobs.job = jobs_data["job"][num]
+        jobs.work_size = jobs_data["work_size"][num]
+        jobs.collaborations = jobs_data["collaborations"][num]
+        jobs.start_date = jobs_data["start_date"][num]
+        jobs.is_finished = jobs_data["is_finished"][num]
+        db_sess.add(jobs)
     db_sess.commit()
     # app.run()
 
